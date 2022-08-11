35b02a62316076600a9ec02ba7a5848efaff8198
Marcel Otavio Cerri <32442095+marcelcerri@users.noreply.github.com>
2022-08-11 13:54:17 -0300

    Arquivos Hello.py
    Primeira aula do curso CS50P - Lecture 0 - Functions, Variables
    https://www.youtube.com/watch?v=JP7ITIXGpHk&t=640s

---
+++ hello2.py
@@ -0,0 +1,15 @@
+# Segundo programa - Imprime Hello com o seu nome
+#Entrada de dados - input
+name = input("What's your name? ")
+#Impressão das variáveis
+#Primeira versão
+print("Hello, " + name)
+#Segunda maneira de imprimir
+print("Hello,", name)
+#Terceira maneira de imprimir
+print('Hello, ', end="")
+print(name)
+#Quarta maneira de imprimir
+print(f"Hello, {name}")
+
+#Funções (print, input and comments)
---
+++ hello3.py
@@ -0,0 +1,8 @@
+#Terceiro programa sobre Hello
+name = input("What's your name? ")
+#Remove os espaços em branco na string
+name = name.strip()
+#Coloca em maiusculo o nome
+name = name.title()
+#Imprimi Hello para o ususário
+print(f"Hello, {name}")
